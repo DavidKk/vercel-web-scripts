ea59d2d9a1adf5fd3add59f49cff4c16b832de09
DavidJones <qowera@gmail.com>
2025-02-05 18:15:59 +0800

    fix: check auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { fetchFiles } from '@/app/api/scripts/actions'
+import { checkAccess } from '@/services/auth/access'
 import Editor from './Editor'
 
 export default async function Home() {
+  await checkAccess()
+
   const files = await fetchFiles()
   return <Editor files={files} />
 }
