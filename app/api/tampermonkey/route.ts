7f85240b0820cc07a18819ba3b98e7aa2faf023a
DavidJones <qowera@gmail.com>
2025-02-05 10:53:49 +0800

    feat: add debug mode

---
+++ app/api/tampermonkey/route.ts
@@ -0,0 +1,44 @@
+import { plainText } from '@/initializer/controller'
+import { textInvalidParameters } from '@/initializer/response'
+import { fetchGist, getGistInfo } from '@/services/gist'
+import { clearMeta, extractMeta } from '@/services/tampermonkey'
+import { matchUrl } from '@/utils/url'
+import { EXCLUDED_FILES } from '@/constants/file'
+
+export const GET = plainText(async (req) => {
+  const uri = new URL(req.url)
+  const encodedUrl = uri.searchParams.get('url')
+  if (!encodedUrl) {
+    return textInvalidParameters('url parameter is required')
+  }
+
+  const url = decodeURIComponent(encodedUrl)
+  if (!/^https?:\/\//.test(url)) {
+    return textInvalidParameters('url must start with http:// or https://')
+  }
+
+  const { gistId, gistToken } = getGistInfo()
+  const gist = await fetchGist({ gistId, gistToken })
+  const files = Array.from(
+    (function* () {
+      for (const [file, { content }] of Object.entries(gist.files)) {
+        if (EXCLUDED_FILES.includes(file)) {
+          continue
+        }
+
+        const meta = extractMeta(content)
+        if (!(meta.match && meta.source)) {
+          continue
+        }
+
+        const match = Array.isArray(meta.match) ? meta.match : [meta.match]
+        if (match.some((m) => matchUrl(m, url))) {
+          yield content
+        }
+      }
+    })()
+  )
+
+  const content = files.join('\n')
+  return clearMeta(content)
+})
--- services/tampermonkey/index.ts
+++ services/tampermonkey/index.ts
@@ -67,12 +67,15 @@ export function clearMeta(content: string) {
 }
 
 export interface CreateBannerParams {
+  match: string[]
   scriptUrl: string
   version: string
 }
 
-export function createBanner({ scriptUrl, version }: CreateBannerParams) {
-  return `
+export function createBanner({ match, scriptUrl, version }: CreateBannerParams) {
+  const uri = new URL(scriptUrl)
+  return (content: string) => {
+    return `
 // ==UserScript==
 // @name         Web Script
 // @namespace    http://tampermonkey.net/
@@ -82,22 +85,95 @@ export function createBanner({ scriptUrl, version }: CreateBannerParams) {
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=vercel.com
 // @downloadURL  ${scriptUrl}
 // @updateURL    ${scriptUrl}
-// @match        *://*/*
+${match.map((m) => `// @match        ${m}`).join('\n')}
 // @noframes
+// @connect      ${uri.hostname}
+// @grant        GM_registerMenuCommand
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-function matchUrl(pattern, url) {
-  const regexPattern = pattern.replace(/\\./g, '\\\\.').replace(/\\*/g, '.*')
-  const regex = new RegExp(\`^\${regexPattern}$\`)
-  return regex.test(url)
-}`
+(async () => {
+  'use strict'
+
+  const DEBUG_KEY = '#DebugMode@WebScripts'
+
+  const matchUrl = (pattern, url) => {
+    const regexPattern = pattern.replace(/\\./g, '\\\\.').replace(/\\*/g, '.*')
+    const regex = new RegExp(\`^\${regexPattern}$\`)
+    return regex.test(url)
+  }
+
+  const fetchScript = async (scriptUrl) => {
+    return new Promise((resolve, reject) => {
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: scriptUrl,
+        onload: function (response) {
+          try {
+            if (!(200 <= response.status && response.status < 400)) {
+              throw new Error('Failed to load remote script: ' + response.statusText)
+            }
+
+            const content = response.responseText
+            resolve(content)
+          } catch (error) {
+            reject(new Error('Error executing remote script: ' + error.message))
+          }
+        },
+        onerror: function (error) {
+          reject(new Error('Failed to load remote script:' + error.message))
+        }
+      })
+    })
+  }
+
+  const toggleDebug = (enable = true) => {
+    sessionStorage.setItem(DEBUG_KEY, enable ? '1' : '0')
+  }
+
+  const isDebugMode = () => {
+    const enable = sessionStorage.getItem(DEBUG_KEY)
+    return enable === '1'
+  }
+
+  GM_registerMenuCommand('Edit Script', () => {
+    const uri = new URL('${scriptUrl}')
+    uri.pathname = ''
+
+    window.open(uri.toString(), '_blank')
+  })
+
+  GM_registerMenuCommand('Toggle Debug Mode', () => {
+    const enable = !isDebugMode()
+    toggleDebug(enable)
+    window.location.reload()
+  })
+
+  if (isDebugMode()) {
+    const scriptUrl = '${scriptUrl.replace('.user.js', '')}?url=' + encodeURIComponent(window.location.href)
+    const content = await fetchScript(scriptUrl)
+
+    if (content) {
+      const execute = new Function('window', content)
+      execute(window)
+      return
+    }
+
+    return
+  }
+
+  ${clearMeta(content)}
+})()
+`
+  }
 }
 
-export interface CreateScriptParams extends CreateBannerParams {
+export interface CreateScriptParams extends Omit<CreateBannerParams, 'match'> {
   files: Record<string, string>
 }
 
 export function createUserScript({ scriptUrl, version, files }: CreateScriptParams) {
+  const matches = new Set<string>()
   const parts = Array.from(
     (function* () {
       for (const [file, content] of Object.entries(files)) {
@@ -110,12 +186,16 @@ export function createUserScript({ scriptUrl, version, files }: CreateScriptPara
           continue
         }
 
-        yield `if(${JSON.stringify(meta.match)}.some((m) => matchUrl(m, window.location.href)))${content.trim()}`
+        const match = Array.isArray(meta.match) ? meta.match : [meta.match]
+        match.forEach((match) => matches.add(match))
+
+        const clearedContent = clearMeta(content)
+        yield `if(${JSON.stringify(match)}.some((m) => matchUrl(m, window.location.href))){${clearedContent}}`
       }
     })()
   )
 
-  const banner = createBanner({ scriptUrl, version })
+  const withBanner = createBanner({ match: Array.from(matches), scriptUrl, version })
   const content = parts.join('\n')
-  return (banner + clearMeta(content)).trim()
+  return withBanner(content).trim()
 }
