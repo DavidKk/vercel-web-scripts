81ea8dcfc30c31b746f6c3ed2cfe77fc2d0a36b1
DavidJones <qowera@gmail.com>
2025-09-27 01:15:30 +0800

    feat: add @vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Nav } from './Nav'
@@ -30,6 +31,7 @@ export default function RootLayout(props: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
         <Nav />
         {children}
